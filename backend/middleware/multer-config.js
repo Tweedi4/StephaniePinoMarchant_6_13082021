a784cc727b87ac1928ac461037302d880e8edb4b
Stephanie Pino <stephanievictoria.pino@gmail.com>
2021-09-07 20:02:53 +0200

    modif multer filename, suppr et rename images

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -12,8 +12,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) => {
         const extension = MIME_TYPES[file.mimetype];
-        const name = file.originalname.split(' ').join('_');
-        callback(null, name + Date.now() + '.' + extension);
+        callback(null, Date.now() + '.' + extension);
     }
 });
 
