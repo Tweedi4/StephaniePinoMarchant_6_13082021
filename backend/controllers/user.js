4398a0ff1d829d42c9dcd82bc948cc6e32c3eb7a
Stephanie Pino <stephanievictoria.pino@gmail.com>
2021-09-23 14:57:07 +0200

    Suppression passValidator, pass, ajout regex, img

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,38 +9,42 @@ const User = require('../models/User');
 
 // Créer compte utilisateur
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10) // Execution de l'algorithme de hashage
-    .then(hash => {
-        const user = new User ({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-        .catch(error => res.status(400).json({ error }));
-// Récuperation hash du mdp qui va sauvegarder dans un nouvel user pour ensuite être enregistré dans la db
-    })
-    .catch(error => res.status(500).json({ error }));
+  const regexPasswordHard = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
+  if (regexPasswordHard.test(req.body.password)){
+      
+      bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+          const user = new User({
+              email: req.body.email,
+              password: hash
+          });
+          user.save()
+              .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+              .catch( error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  }
+  else {
+      res.status(400).json({ message : 'Mot de passe invalide, veuillez mettre au minimum 8 caractères, dont 1 majuscule et un nombre'})
+  }
 };
 
 // Connexion à un compte utilisateur
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+  User.findOne({ email: req.body.email })
       .then(user => {
-        if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }
-        // Si l'adresse mail correspond à un user déjà existant
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
-            }
-            // Si le mot de pass et le hash correspondent
-            res.status(200).json({
-              userId: user._id, // Envoie réponse 200 contenant l'ID user et un token
-              token: jwt.sign(
+          if(!user) {
+              return res.status(401).json({ message: 'Utilisateur non trouvé !'});
+          }
+          bcrypt.compare(req.body.password, user.password)
+              .then(valid => {
+                  if (!valid) {
+                      return res.status(401).json({ message: 'Mot de passe incorrect !'});
+                  }
+                  res.status(200).json({
+                      userId: user._id,
+                      token: jwt.sign(
                 { userId: user._id },
                 'RANDOM_TOKEN_SECRET',
                 { expiresIn: '24h' }
